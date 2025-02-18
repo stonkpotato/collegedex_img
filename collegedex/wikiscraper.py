c9b619938eded410cdf3d5dccbe2c4b51eddca80
Praneel Samal <38089107+stonkpotato@users.noreply.github.com>
2025-02-18 15:07:30 -0800

    actually img work thank :)

--- collegedex/wikiscraper.py
+++ collegedex/wikiscraper.py
@@ -18,23 +18,27 @@ data = collegedata[0:10]
 driver.implicitly_wait(30)
 
 driver.get("https://en.wikipedia.org/wiki/University_of_Houston–Clear_Lake")
-time.sleep(1)
+time.sleep(1.5)
 
 for i in range(len(data)):
     if data[i][1] != "":
         searchbar = driver.find_elements(By.CLASS_NAME, "cdx-text-input__input")[0]
         searchbar.send_keys(data[i][2])
-        button = driver.find_elements(By.XPATH, "//button[contains(text(), 'Search')]")[0]
-        print(button)
-        button.click()
-        time.sleep(0.5)
+        time.sleep(2)
+        listitem = driver.find_elements(By.CLASS_NAME, "cdx-menu__listbox")[0]
+        it = listitem.find_elements(By.TAG_NAME, "li")[0]
+        a = it.find_element(By.TAG_NAME, "a")
+        a.click()
+        time.sleep(2)
         table = driver.find_elements(By.CSS_SELECTOR, ".infobox.vcard")[0]
-        grids = table.find_elements(By.TAG_NAME, "tr")
-        img = grids[0].find_element(By.TAG_NAME, "img")
-        imgurl = 'https:' + img.get_attribute('src')
+        grids = table.find_elements(By.CLASS_NAME, "infobox-image")[0]
+        img = grids.find_element(By.TAG_NAME, "img")
+        imgurl = img.get_attribute('src')
+        spl = imgurl.split('.')[-1]
         img_data = requests.get(imgurl).content
-        with open(f'${i}.jpg', 'wb') as handler:
+        with open(f'./images/{data[i][1]}.{spl}', 'wb') as handler:
             handler.write(img_data)
+        time.sleep(2)
 
 # print(collegedata)
 
